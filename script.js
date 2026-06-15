bddc3cf7f78cd4253bb876e46fb32f6a02b0072e
LMLeo1 <leo.million44@gmail.com>
2026-06-15 15:20:56 +0200

    Added script

---
+++ script.js
@@ -0,0 +1,10 @@
+const cpuName = "AMD Ryzen 9 7950X"; 
+// String
+let cpuPrice = 420;
+// Number
+let isInStock = true;
+// Boolean
+console.log("---- CPU INFO ----");
+console.log("Name:", cpuName);
+console.log("Price ($):", cpuPrice);
+console.log("Available:", isInStock);
