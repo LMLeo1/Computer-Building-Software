912dc74b22fe3866c68769179ead4efd272ace8e
LMLeo1 <leo.million44@gmail.com>
2026-06-15 16:35:53 +0200

    Working prices

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cpuName = "AMD Ryzen 9 7950X"; 
+const cpuName = "AMD Ryzen 9 7950X";
 // String
 let cpuPrice = 420;
 // Number
@@ -8,6 +8,12 @@ console.log("---- CPU INFO ----");
 console.log("Name:", cpuName);
 console.log("Price ($):", cpuPrice);
 console.log("Available:", isInStock);
+function calculateTotalPrice(cpuPrice, gpuPrice, ramPrice, ssdPrice) {
+    // Inside the machine: sum all parameters together
+    const currentTotal = cpuPrice + gpuPrice + ramPrice + ssdPrice;
+    // Shipping the final product back out
+    return currentTotal;
+}
 const buildOneTotal = calculateTotalPrice(350, 850, 120, 90);
 console.log("Client Build 1 Total:", "$" + buildOneTotal);
 const buildTwoTotal = calculateTotalPrice(120, 200, 60, 45);
