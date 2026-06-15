c98f3861aa5ea1ab428ca1762971739bc671726e
LMLeo1 <leo.million44@gmail.com>
2026-06-15 17:26:51 +0200

    Edit

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const calculateButton = document.getElementById("calc-btn");
-const cpuSelect = document.getElementById("cpu-select");
-const gpuSelect = document.getElementById("gpu-select");
-const totalDisplay = document.getElementById("total-display");
-calculateButton.addEventListener("click", function() { 
-    const selectedCpuPrice = Number(cpuSelect.value);
-    const selectedGpuPrice = Number(gpuSelect.value);
-    const totalPrice = selectedCpuPrice + selectedGpuPrice;
-    totalDisplay.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
-});
- const gpuCatalog = [
+const gpuCatalog = [
 { name: "AMD RX 7600 XT", price: 330, powerRequired: 600 }, // Index 0
 { name: "NVIDIA RTX 4070 Super", price: 650, powerRequired: 650 }, // Index 1
 { name: "NVIDIA RTX 4080 Super", price: 1050, powerRequired: 750 } // Index 2
