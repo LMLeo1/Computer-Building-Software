1bf6c00689955892bf7efe30ffd5487a4f2ee0fd
LMLeo1 <leo.million44@gmail.com>
2026-06-16 14:01:12 +0200

    Added Style to the project

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const calculateButton = document.getElementById("calc-btn");
 const cpuSelect = document.getElementById("cpu-select");
 const gpuSelect = document.getElementById("gpu-select");
+const psuSelect = document.getElementById("psu-select");
 const totalDisplay = document.getElementById("total-display");
-calculateButton.addEventListener("click", function() {
+const gpuCatalog = [
+    { name: "AMD RX 7600 XT", price: 330, powerRequired: 600 },       // Index 0
+    { name: "NVIDIA RTX 4070 Super", price: 650, powerRequired: 650 }, // Index 1
+    { name: "NVIDIA RTX 4080 Super", price: 1050, powerRequired: 750 }, // Index 2
+    { name: "NVIDIA RTX 3060", price: 500, powerRequired: 550 } // Index 3
+];
+
+    const handleCalculation = function() {
     const selectedCpuPrice = Number(cpuSelect.value);
     const selectedGpuPrice = Number(gpuSelect.value);
-    const totalPrice = selectedCpuPrice + selectedGpuPrice;
+    const selectedPsuPrice = Number(psuSelect.value);
+    
+    const totalPrice = selectedCpuPrice + selectedGpuPrice + selectedPsuPrice;
     totalDisplay.textContent = `Total Price: $${totalPrice.toFixed(2)}`; 
-    totalDisplay.innerText = "$" + totalPrice; 
-});
+};
+    calculateButton.addEventListener("click", handleCalculation); 
+    
