21a601507aae4afddc83759f58804ca5290864f9
LMLeo1 <leo.million44@gmail.com>
2026-06-16 17:37:37 +0200

    Added dedicated file for cpu

--- gpu-data.js
+++ gpu-data.js
@@ -3,7 +3,7 @@ const gpuCatalog = [
     { name: "NVIDIA RTX 5080", price: 999, powerRequired: 850 }, // Index 1
     { name: "NVIDIA RTX 5070", price: 549, powerRequired: 600 }, // Index 2
     { name: "NVIDIA RTX 5070 Ti", price: 549, powerRequired: 750 }, // Index 3
-    {name: "NVIDIA RTX 5060", price: 300, powerRequired: 500}, // Index 4
+    { name: "NVIDIA RTX 5060", price: 300, powerRequired: 500}, // Index 4
     { name: "NVIDIA RTX 4080 Super", price: 1050, powerRequired: 750 },
-    {name: "NVIDIA RTX 4070 Super", price: 549, powerRequired: 600}, // Index 5
+    { name: "NVIDIA RTX 4070 Super", price: 549, powerRequired: 600}, // Index 5
 ];
--- script.js
+++ script.js
@@ -3,11 +3,7 @@ const cpuSelect = document.getElementById("cpu-select");
 const gpuSelect = document.getElementById("gpu-select");
 const psuSelect = document.getElementById("psu-select");
 const totalDisplay = document.getElementById("total-display");
-const cpuCatalog = [
-    { name: "Intel Core i7-13700K", price: 350, powerRequired: 125 },
-    { name: "AMD Ryzen 9 7950X", price: 450, powerRequired: 120 }
     
-];
 
     const handleCalculation = function() {
     const selectedCpuPrice = Number(cpuSelect.value);
@@ -18,4 +14,3 @@ const cpuCatalog = [
     totalDisplay.textContent = `$${totalPrice.toFixed(2)}`; 
 };
     calculateButton.addEventListener("click", handleCalculation); 
-    
